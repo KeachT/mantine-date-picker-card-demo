46b3cad05c97c6902f9eeb48fa6b4c3fa589f96c
KeachT <503.b03@gmail.com>
2023-07-17 16:04:35 +0900

    add DatePickerCard

---
+++ src/components/DatePickerCard/DatePickerCard.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import { Box, Card, Grid, Text } from '@mantine/core'
+import { DateValue } from '@mantine/dates'
+import { useDisclosure } from '@mantine/hooks'
+import { BoxMultiple, ChevronLeft, ChevronRight } from 'tabler-icons-react'
+import { createCardStyle } from './utils'
+import { DatePickerModal } from './DatePickerModal'
+
+export function DatePickerCard() {
+  const [currentDate, setCurrentDate] = useState<DateValue>(new Date())
+  const [opened, { open, close }] = useDisclosure(false)
+  const { classes } = createCardStyle()
+
+  const currentDateChange = (amount: number) => {
+    const oldDate = currentDate || new Date()
+    const newDate = new Date(oldDate)
+    newDate.setDate(oldDate.getDate() + amount)
+    setCurrentDate(newDate)
+  }
+
+  const handleClickIconChevronRight = () => {
+    currentDateChange(1)
+  }
+
+  const handleClickIconChevronLeft = () => {
+    currentDateChange(-1)
+  }
+
+  return (
+    <Box>
+      <Card radius="lg" withBorder>
+        <Grid align="baseline" justify="center" mt={2} mb={10}>
+          <Text weight={100} size="md">
+            Current Day:
+          </Text>
+          <Text weight={200} size="xl" ml={8}>
+            {currentDate?.toLocaleDateString()}
+          </Text>
+        </Grid>
+        <Grid align="baseline" justify="center" mt={2}>
+          <Box mr={20} onClick={handleClickIconChevronLeft}>
+            <ChevronLeft size={20} strokeWidth={1} className={classes.button} />
+          </Box>
+          <Box mr={20} onClick={open}>
+            <BoxMultiple
+              size={20}
+              strokeWidth={0.5}
+              className={classes.button}
+            />
+          </Box>
+          <Box onClick={handleClickIconChevronRight}>
+            <ChevronRight
+              size={20}
+              strokeWidth={1}
+              className={classes.button}
+            />
+          </Box>
+        </Grid>
+      </Card>
+
+      {opened && (
+        <DatePickerModal
+          opened={opened}
+          close={close}
+          currentDate={currentDate}
+          setCurrentDate={setCurrentDate}
+        />
+      )}
+    </Box>
+  )
+}
---
+++ src/components/DatePickerCard/DatePickerModal.tsx
@@ -0,0 +1,31 @@
+import { Dispatch, SetStateAction } from 'react'
+import { Modal } from '@mantine/core'
+import { DatePicker, DateValue } from '@mantine/dates'
+
+type DatePickerModalProps = {
+  opened: boolean
+  close: () => void
+  currentDate: DateValue
+  setCurrentDate: Dispatch<SetStateAction<DateValue>>
+}
+
+export function DatePickerModal({
+  opened,
+  close,
+  currentDate,
+  setCurrentDate,
+}: DatePickerModalProps) {
+  const handleDateChange = (date: DateValue) => {
+    setCurrentDate(date)
+    close()
+  }
+
+  return (
+    <Modal opened={opened} onClose={close} size="auto">
+      <DatePicker
+        value={currentDate}
+        onChange={(selectedDate) => handleDateChange(selectedDate)}
+      />
+    </Modal>
+  )
+}
